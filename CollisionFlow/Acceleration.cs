cb300e327a71bfdda3815e913141d0f01eff8410
PavelBfl <33580861+PavelBfl@users.noreply.github.com>
2021-10-09 18:43:28 +0500

    Реализованы основные формулы расчёта ускорения

---
+++ CollisionFlow/Acceleration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace CollisionFlow
+{
+	public struct Course
+	{
+		public Course(Vector<double> v, Vector<double> a)
+		{
+			V = v;
+			A = a;
+		}
+		public Vector<double> V { get; }
+		public Vector<double> A { get; }
+
+		public Course Offset(double time) => new Course(V + A * time, A);
+	}
+	public class Acceleration
+	{
+		private static bool IsZero(double value) => Math.Abs(value) < 0.0000001;
+		public static double? GetTime(double min, double vMin, double aMin, double max, double vMax, double aMax)
+		{
+			var v = vMin - vMax;
+			var a = aMin - aMax;
+
+			if ((IsZero(v) || v < 0) && (IsZero(a) || a < 0))
+			{
+				return null;
+			}
+
+			var s = max - min;
+
+			var d = (v * v) - 2 * a * (-s);
+			if (IsZero(d))
+			{
+				var result = -v / a;
+				return result;
+			}
+			else if (d > 0)
+			{
+				var result1 = (-v + Math.Sqrt(d)) / a;
+				if (result1 > 0)
+				{
+					return result1;
+				}
+
+				var result2 = (-v - Math.Sqrt(d)) / a;
+				if (result2 > 0)
+				{
+					return result2;
+				}
+
+				throw new InvalidOperationException();
+			}
+			else
+			{
+				return null;
+			}
+		}
+		public static double Offset(double time, double value, double v, double a)
+		{
+			return value + v * time + (a * time * time) / 2;
+		}
+	}
+}
--- GuiTest/MainWindow.xaml.cs
+++ GuiTest/MainWindow.xaml.cs
@@ -30,86 +30,55 @@ namespace GuiTest
 		{
 			InitializeComponent();
 
-			const double WEIGHT_MAX = 10;
-			const double HEIGHT_MAX = 10;
-			const double SPEED_MAX = 0;
-			const int ROWS_COUNT = 10;
-			const int COLUMNS_COUNT = 10;
-			const double GLOBAL_OFFSET = 300;
+			const double Y = 100;
 
-			var random = new Random(1);
-			for (int iRow = 0; iRow < ROWS_COUNT; iRow++)
+			var x1 = 50d;
+			var v1 = 5d;
+			var a1 = -0.1d;
+
+			var x2 = 500d;
+			var v2 = -10d;
+			var a2 = 0.1d;
+
+			DrawPoint(new Point(x1, Y), Brushes.Red, 7);
+			DrawPoint(new Point(x2, Y), Brushes.Red, 7);
+
+			var time = Acceleration.GetTime(x1, v1, a1, x2, v2, a2);
+
+			if (time is not null)
 			{
-				for (int iColumn = 0; iColumn < COLUMNS_COUNT; iColumn++)
+				var collisionX1 = Acceleration.Offset(time.Value, x1, v1, a1);
+				var collisionX2 = Acceleration.Offset(time.Value, x2, v2, a2);
+
+				DrawPoint(new Point(collisionX1, Y + 10), Brushes.Green);
+				DrawPoint(new Point(collisionX2, Y + 20), Brushes.Green);
+
+				const double TIME_STEP = 1;
+				var timeCounter = TIME_STEP;
+				while (timeCounter < time.Value)
 				{
-					var offsetX = GLOBAL_OFFSET + iColumn * WEIGHT_MAX;
-					var offsetY = iRow * HEIGHT_MAX;
-					var centerX = offsetX + WEIGHT_MAX / 2;
-					var centerY = offsetY + HEIGHT_MAX / 2;
-					var points = PolygonBuilder.RegularPolygon((Math.Min(WEIGHT_MAX, HEIGHT_MAX) - 1) / 2, random.Next(3, 10))
-						.Select(x => new Vector128(x.X + centerX, x.Y + centerY))
-						.ToArray();
+					var offset1 = Acceleration.Offset(timeCounter, x1, v1, a1);
+					var offset2 = Acceleration.Offset(timeCounter, x2, v2, a2);
 
-					var body = new Body(BodyDispatcher.Dispatcher, points, new Vector128(random.NextDouble() * SPEED_MAX, random.NextDouble() * SPEED_MAX))
-					{
-						Pull = new Vector128(0, 0.1),
-						Bounce = 0.95,
-					};
-					BodyDispatcher.Bodies.Add(body);
-				}
+					DrawPoint(new Point(offset1, Y), Brushes.Pink);
+					DrawPoint(new Point(offset2, Y), Brushes.Pink);
+
+					timeCounter += TIME_STEP;
+				} 
 			}
-
-			var bottom = new Body(BodyDispatcher.Dispatcher, new Vector128[]
-			{
-				new(0, -1000),
-				new(0, 300),
-				new(700, 300),
-				new(700, -1000),
-				new(710, -1000),
-				new(710, 310),
-				new(-10, 310),
-				new(-10, -1000),
-			})
-			{
-				Weight = 1000000000000,
-			};
-			BodyDispatcher.Bodies.Add(bottom);
-
-			var bod = new Body(BodyDispatcher.Dispatcher, new Vector128[]
-			{
-				new(350, 200),
-				new(400, 250),
-				new(300, 250),
-			})
-			{
-				Weight = 1000000000000,
-			};
-			BodyDispatcher.Bodies.Add(bod);
-
-			foreach (var body in BodyDispatcher.Bodies)
-			{
-				var bodyVm = new BodyVm(body);
-				Bodyes.Add(bodyVm);
-				CnvRoot.Children.Add(bodyVm.Polygon);
-			}
-
-			DispatcherTimer = new DispatcherTimer();
-			DispatcherTimer.Tick += FrameUpdate;
-			DispatcherTimer.Interval = TimeSpan.FromMilliseconds(1000 / 60);
-			DispatcherTimer.Start();
 		}
 
-		private void FrameUpdate(object? sender, EventArgs e)
+		private void DrawPoint(Point position, Brush color, double size = 5)
 		{
-			BodyDispatcher.Offset(1);
-			foreach (var bodyVm in Bodyes)
+			var ellipse = new Ellipse()
 			{
-				bodyVm.Refresh();
-			}
+				Width = size,
+				Height = size,
+				Fill = color,
+			};
+			Canvas.SetTop(ellipse, position.Y - size / 2);
+			Canvas.SetLeft(ellipse, position.X - size / 2);
+			CnvRoot.Children.Add(ellipse);
 		}
-
-		private List<BodyVm> Bodyes { get; } = new();
-		private BodyDispatcher BodyDispatcher { get; } = new();
-		public DispatcherTimer DispatcherTimer { get; }
 	}
 }
